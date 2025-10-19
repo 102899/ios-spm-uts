bcc9d4d6f2ec60d312fe0f9681ab0d4cb125298c
is_boy <1028993962@qq.com>
2025-10-19 20:21:12 +0800

    update

--- Package.swift
+++ Package.swift
@@ -1,26 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-//
-// Copyright OpenJS Foundation and other contributors, https://openjsf.org
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 import PackageDescription
 
 let package = Package(
@@ -35,9 +13,34 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(
+        .target(
             name: "NativeScript",
-            path: "NativeScript.xcframework"
+            dependencies: [],
+            path: "Sources/NativeScript",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath("include/runtime"),
+                .define("NS_ENABLE_RUNTIME", to: "1"),
+            ],
+            cxxSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath("include/runtime"),
+                .define("NS_ENABLE_RUNTIME", to: "1"),
+            ],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx)
+            ],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("JavaScriptCore"),
+                .linkedLibrary("c++"),
+                .linkedLibrary("z"),
+                .linkedLibrary("sqlite3"),
+            ]
         )
-    ]
+    ],
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx17
 )
